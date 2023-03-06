cae945cde2466a554adfac87e2c981abefdd9dde
Wayne Buchner <wayne@waybu.com>
2023-03-07 09:23:16 +1100

    Initial project commit

---
+++ Newspaper/Newspaper/NewspaperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewspaperApp.swift
+//  Newspaper
+//
+//  Created by Wayne Buchner on 7/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct NewspaperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
